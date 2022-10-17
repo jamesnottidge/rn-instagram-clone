e0f91136b459b0d81f65316dbf718521a7cb6007
James Nottidge <freshice57@gmail.com>
2022-10-17 13:47:52 +0100

    Bottom Tabs build 1

--- screens /HomeScreen.js
+++ screens /HomeScreen.js
@@ -4,6 +4,8 @@ import Header from "../components /home/Header";
 import Stories from "../components /home/Stories";
 import Post from "../components /home/Post";
 import { POSTS } from "../data/posts";
+import BottomTabs from "../components /home/BottomTabs";
+import { bottomTabIcons } from "../components /home/BottomTabs";
 
 const HomeScreen = () => {
   return (
@@ -15,6 +17,7 @@ const HomeScreen = () => {
           <Post key={index} post={post} />
         ))}
       </ScrollView>
+      <BottomTabs icons={bottomTabIcons} />
     </SafeAreaView>
   );
 };
