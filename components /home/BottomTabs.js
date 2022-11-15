9f4735b07c47945b57f83b6374a6f5a829f7ce4e
James Nottidge <freshice57@gmail.com>
2022-11-15 22:26:22 +0100

    Bottom Tab positioning fixed

--- components /home/BottomTabs.js
+++ components /home/BottomTabs.js
@@ -68,7 +68,7 @@ const styles = StyleSheet.create({
   wrapper: {
     position: "absolute",
     width: "100%",
-    bottom: "0",
+    bottom: "0%",
     zIndex: 999,
     backgroundColor: "#000",
   },
